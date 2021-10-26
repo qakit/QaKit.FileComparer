e43b979a665759e63397d2ca52b127db00fce545
AndrewEgorov <anridegod@yandex.ru>
2021-10-26 12:58:51 +0700

    Added one more test stub method to compare and not throw errors if number of images in difrectory not equal to each other

---
+++ src/Image/ImageCompareResult.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace QaKit.FileComparer.Image
+{
+	public class ImageCompareResult
+	{
+		public FileInfo ExpectedImage { get; set; } = null;
+		public FileInfo ActualImage { get; set; } = null;
+		public FileInfo DiffImage { get; set; } = null;
+	}
+}
--- src/Image/ImageDocumentComparer.cs
+++ src/Image/ImageDocumentComparer.cs
@@ -16,11 +16,13 @@ namespace QaKit.FileComparer.Image
 			using (var expectedImage = new MagickImage(expectedImageFile.FullName))
 			using (var actualImage = new MagickImage(actualImageFile.FullName))
 			{
-				return ComparingFilesAreEqual(expectedImage, actualImage, new FileInfo(actualImageFile.FullName + "_comparingResults.png"));
+				return ComparingFilesAreEqual(expectedImage, actualImage,
+					new FileInfo(actualImageFile.FullName + "_comparingResults.png"));
 			}
 		}
 
-		public static bool ComparingFilesAreEqual(FileInfo expectedImageFile, FileInfo actualImageFile, FileInfo outputDiffFile)
+		public static bool ComparingFilesAreEqual(FileInfo expectedImageFile, FileInfo actualImageFile,
+			FileInfo outputDiffFile)
 		{
 			using (var expectedImage = new MagickImage(expectedImageFile.FullName))
 			using (var actualImage = new MagickImage(actualImageFile.FullName))
@@ -29,7 +31,8 @@ namespace QaKit.FileComparer.Image
 			}
 		}
 
-		public static bool ComparingFilesAreEqual(MagickImage expectedImageFile, MagickImage actualImageFile, FileInfo outputDiffFile)
+		public static bool ComparingFilesAreEqual(MagickImage expectedImageFile, MagickImage actualImageFile,
+			FileInfo outputDiffFile)
 		{
 			using (var outputErrorImage = new MagickImage())
 			{
@@ -49,23 +52,23 @@ namespace QaKit.FileComparer.Image
 				var outputErrorFile = outputDiffFile.FullName;
 				outputErrorImage.Write(outputErrorFile);
 				return false;
-
 			}
 		}
 
-		public static bool AllImageFilesInDirectoryAreEqual(DirectoryInfo expectedImagesFolder, DirectoryInfo actualImagesFolder)
+		public static bool AllImageFilesInDirectoryAreEqual(DirectoryInfo expectedImagesFolder,
+			DirectoryInfo actualImagesFolder)
 		{
-			var actualImageFiles = actualImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).
-					Where(file => file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
-								  file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
-								  file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
-								  file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase));
+			var actualImageFiles = actualImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).Where(file =>
+				file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase));
 
-			var expectedImageFiles = expectedImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).
-					Where(file => file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
-								  file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
-								  file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
-								  file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase));
+			var expectedImageFiles = expectedImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).Where(
+				file => file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
+						file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
+						file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
+						file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase));
 
 			bool allFilesAreEqual = true;
 			if (actualImageFiles.Count() != expectedImageFiles.Count())
@@ -94,19 +97,20 @@ namespace QaKit.FileComparer.Image
 		/// <param name="expectedImagesFolder"></param>
 		/// <param name="actualImagesFolder"></param>
 		/// <returns>List of tuples where Tuple[0] expected file, Tuple[1] actual file, Tuple[2] is a diff file or null in case if files are equal to each other</returns>
-		public static List<Tuple<FileInfo, FileInfo, FileInfo>> AllImagesInDirectoryAreEqual(DirectoryInfo expectedImagesFolder, DirectoryInfo actualImagesFolder)
+		public static List<Tuple<FileInfo, FileInfo, FileInfo>> AllImagesInDirectoryAreEqual(
+			DirectoryInfo expectedImagesFolder, DirectoryInfo actualImagesFolder)
 		{
-			var actualImageFiles = actualImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).
-				Where(file => file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
-				              file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
-				              file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
-				              file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase));
+			var actualImageFiles = actualImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).Where(file =>
+				file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase));
 
-			var expectedImageFiles = expectedImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).
-				Where(file => file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
-				              file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
-				              file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
-				              file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase));
+			var expectedImageFiles = expectedImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).Where(
+				file => file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
+						file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
+						file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
+						file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase));
 
 			if (actualImageFiles.Count() != expectedImageFiles.Count())
 			{
@@ -124,6 +128,85 @@ namespace QaKit.FileComparer.Image
 				result.Add(!allFilesAreEqual
 					? new Tuple<FileInfo, FileInfo, FileInfo>(expectedImageFile, actualImageFile, diffFile)
 					: new Tuple<FileInfo, FileInfo, FileInfo>(expectedImageFile, actualImageFile, null));
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Compare all images in specified folders and return information about files (expected, actual and diff if any)
+		/// </summary>
+		/// <param name="expectedImagesFolder"></param>
+		/// <param name="actualImagesFolder"></param>
+		public static List<ImageCompareResult> AllImagesInDirectoryAreEqual2(DirectoryInfo expectedImagesFolder,
+			DirectoryInfo actualImagesFolder)
+		{
+			var actualImageFiles = actualImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).Where(file =>
+				file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+			var expectedImageFiles = expectedImagesFolder.EnumerateFiles("*.*", SearchOption.AllDirectories).Where(file =>
+				file.Extension.Equals(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".png", StringComparison.InvariantCultureIgnoreCase) ||
+				file.Extension.Equals(".gif", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+			//if (actualImageFiles.Count() != expectedImageFiles.Count())
+			//{
+			//	throw new Exception("Number of images in directories are not equal to each other");
+			//}
+
+			List<ImageCompareResult> result = new List<ImageCompareResult>();
+
+			if (actualImageFiles.Count >= expectedImageFiles.Count)
+			{
+				foreach (FileInfo actualImageFile in actualImageFiles)
+				{
+					var expectedImageFile = expectedImageFiles.FirstOrDefault(s => s.Name == actualImageFile.Name);
+					if (expectedImageFile == null)
+					{
+						result.Add(new ImageCompareResult
+						{
+							ActualImage = actualImageFile
+						});
+						continue;
+					}
+					var diffFile = new FileInfo(actualImageFile.FullName + "_comparingResults.png");
+					var allFilesAreEqual = ComparingFilesAreEqual(expectedImageFile, actualImageFile, diffFile);
+					var currentResult = new ImageCompareResult
+					{
+						ActualImage = actualImageFile,
+						ExpectedImage = expectedImageFile,
+						DiffImage = !allFilesAreEqual ? diffFile : null
+					};
+					result.Add(currentResult);
+				}
+			}
+			else
+			{
+				foreach (FileInfo expectedImageFile in expectedImageFiles)
+				{
+					var actualImageFile = actualImageFiles.FirstOrDefault(s => s.Name == expectedImageFile.Name);
+					if (actualImageFile == null)
+					{
+						result.Add(new ImageCompareResult
+						{
+							ExpectedImage = expectedImageFile
+						});
+						continue;
+					}
+					var diffFile = new FileInfo(actualImageFile.FullName + "_comparingResults.png");
+					var allFilesAreEqual = ComparingFilesAreEqual(expectedImageFile, actualImageFile, diffFile);
+					var currentResult = new ImageCompareResult
+					{
+						ActualImage = actualImageFile,
+						ExpectedImage = expectedImageFile,
+						DiffImage = !allFilesAreEqual ? diffFile : null
+					};
+					result.Add(currentResult);
+				}
 			}
 
 			return result;
